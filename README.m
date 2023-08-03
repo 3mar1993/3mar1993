b03bc8b72f21000df973c1c03197d77a444fc032
Yamuna <139667629+3mar1993@users.noreply.github.com>
2023-08-04 00:49:29 +0400

    Food court order-simple beginners project

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
-# foodcourtorder-py
+#foodcortorder-py simple practice code for beginners 
 Config files for my GitHub profile.
 Using list& dictionary datatypes to get information 
 if condition for ger food order
